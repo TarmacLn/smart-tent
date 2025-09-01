c19ccfc406fe40e6bb08589c37c839d66128ff16
Tarmac <iwannandrianou@gmail.com>
2025-09-01 17:05:57 +0300

    small update theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,27 @@ import { observer } from 'mobx-react-lite';
 import './App.less';
 import { Outlet } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { lightBlue } from '@mui/material/colors';
 
 const App: React.FC = () => {
 
     const theme = createTheme({
+        palette: {
+            primary: {
+                main: lightBlue[600],
+                contrastText: '#fff',
+            },
+            mode: 'light',
+        },
+        components: {
+            MuiPaper: {
+                styleOverrides: {
+                    root: {
+                        backgroundColor: '#e4e4e4ff',
+                    },
+                },
+            },
+        }
     });
 
     return (
